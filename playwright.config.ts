f8a425b09edcaaf9527dd66bbba7a28826348e86
Bassuprince <109868579+Bassuprince@users.noreply.github.com>
2026-08-19 10:08:13 +0530

    Adjust worker count based on CI environment

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 1,
-  workers: process.env.CI ? 4 : 2,
+  workers: process.env.CI ? 2 : 0,
   reporter: [
     ['list'],
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
